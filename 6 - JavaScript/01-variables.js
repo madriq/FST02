a8842d6c6c168dd09bb72725d9f5218d4d5f4b10
madriq <azriq@live.com.my>
2024-10-08 17:07:08 +0800

    added day 6

---
+++ 6 - JavaScript/01-variables.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+// This is a single-line comment
+
+// This is a 
+// multi-line 
+// comment
+// Variables
+// let - is a mutable/changeable (block-scope)
+// var - is a mutable/changeable (global-scope)
+// const - unmutable/ unchangeable
+// Camel casing 
+let myFirstName = "Mohamad";
+let firstName = "Azriq";
+firstName = "Azriq Azidin";
+console.log(firstName);
+var lastName = "Azidin";
+lastName = "Azriq Azidin";
+console.log(lastName);
+const birthDate = "01-01-2001";
+// birthDate = "01-02-1997"
+console.log(birthDate);
+ 
+
