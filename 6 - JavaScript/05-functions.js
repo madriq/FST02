42824db227470123e790925851872b71d07abe8f
madriq <azriq@live.com.my>
2024-10-09 17:04:28 +0800

    DOM Manipulation

---
+++ 6 - JavaScript/05-functions.js
@@ -0,0 +1,34 @@
+// Functions
+// Reusable blocks of code that can be executed whenever called/needed.
+
+// 1. Function Declaration
+function greet(){
+    console.log("Hello welcome to the world of functions!");
+    console.log("Let's Goo!!");
+}
+// Function Call
+gfunction greet(){
+    console.log("Hello Welcometo the world of functions!");
+    console.log("Let's Goo!!");
+})// 1. Function Declaration
+function greet(){
+    console.log("Hello welcome to the world of functions!");
+    console.log("Let's Goo!!");
+}
+// Function Call
+greet();
+greet();
+// 2. Functions with parameters
+// Parameters are placeholders
+// Arguments - values passed to a function's parameters
+function sum(num1, num2){
+    console.log("Calculating..");
+    return num1+num2;
+}
+console.log(sum(10,50));
+}function sum(num1, num2){
+    console.log("Calculating..");
+    return num1+num2;
+}
+let a = sum(10,15);
+console.log(a);
